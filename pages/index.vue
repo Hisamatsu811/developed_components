5ee27e0d0f9db66f232323d04bcf3c7f0cbf654a
Hisamatsu811 <40262818+Hisamatsu811@users.noreply.github.com>
2021-07-17 09:38:42 +0900

    微修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <AccordionMenu />
+  <div>
+    <p>aaaaaaaaaaaaaaaaa</p>
+    <p v-for="list in tag_lists" :key="list.id">{{ list }}</p>
+    <AccordionMenu class="tagmodal-checklist_category" v-for="list in tag_lists" :key="list.id">
+      <button slot="title" class="tagmodal-checklist_category_title">
+        <p>{{ list.type }}</p>
+      </button>
+      <li class="tagmodal-checklist_category_column" slot="content">
+        <ul v-for="tag in list.tags" :key="tag.id" >
+          <li>
+            <input type="checkbox" :id="tag.id" :value="tag" />
+            <label :for="tag.id">
+              <img :src="'https://ptera-publish.topaz.dev/' + tag.icon_path" :alt="tag.id" />
+              {{ tag.id }}
+            </label>
+          </li>
+        </ul>
+      </li>
+    </AccordionMenu>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,6 +26,30 @@ import Vue from 'vue'
 import AccordionMenu from '~/components/AccordionMenu.vue'
 
 export default Vue.extend({
+  data() {
+    return {
+      tag_lists: [
+      {
+        type: 'aaa',
+        tags: [
+          'aaa1', 'aaa2', 'aaa3'
+        ]
+      },
+      {
+        type: 'bbb',
+        tags: [
+          'bbb1', 'bbb2', 'bbb3'
+        ]
+      },
+      {
+        type: 'ccc',
+        tags: [
+          'ccc1', 'ccc2', 'ccc3'
+        ]
+      },
+    ]
+    }
+  },
   components: {
     AccordionMenu
   }
